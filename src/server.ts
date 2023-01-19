a6d8272436ff054db6077558cdb74eca985756d7
luaanaapereiraa <luanapdsantos@gmail.com>
2023-01-18 21:51:29 -0300

    Build: Add TypeScript and Prisma

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import Fastify from "fastify";
+
+
+const app = Fastify()
+
+app.get('/', () => {
+    return 'Hello Word'
+})
+
+app.listen({
+    port: 3333,
+}).then(()=>{
+    console.log('HTTP Server running!')
+})
